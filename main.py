c23d4a25297cc26bf5ecdc17a19a3a861194ea63
deostroll <deostroll@gmail.com>
2020-04-18 21:06:28 +0530

    added a dictionary for tracking residual terms

--- main.py
+++ main.py
@@ -1,21 +1,5 @@
 import gmpy2
-
-class PIter:
-  def __init__(self):
-    self.num_list = []
-
-  def __iter__(self):
-    self.n = 1
-    return self
-
-  def __next__(self):
-    while not is_prime(self.n):
-      self.n += 1
-    
-    self.num_list.append(self.n)
-    num = self.n
-    self.n += 1
-    return num
+from pprint import pprint as pp
 
 def sqrt_test(N):
   n = N
@@ -49,30 +33,6 @@ class RIterator:
         return n
       n = gmpy2.add(n, 1)
 
-class P2n:
-  def __init__(self, start):
-    self.start = start
-    self.iterator = None
-    
-  def __iter__(self):
-    if gmpy2.is_prime(self.start):
-      self.s = 0
-    else:
-      raise Exception("Invalid prime:", self.start)
-    return self
-  def __next__(self):
-    result = self.start ** 2 ** self.s
-    # result = gmp
-    self.s += 1
-    return result
-  def get_iterator(self):
-    if self.iterator is None:
-      self.iterator = iter(self)
-    return self.iterator
-  def reset_iterator(self):
-    self.iterator = iter(self)
-    return self.iterator
-
 class P2n_1:
   def __init__(self, start):
     self.start = start
@@ -97,18 +57,29 @@ class P2n_1:
     self.iterator = iter(self)
     return self.iterator
 
-def gen_terms(iterator, terms):
-  return [ next(iterator) for x in range(0, terms)]
+class RDict(dict):
+  def __init__(self, *args):
+    dict.__init__(self, args)
+    ri = RIterator()
+    _iter = iter(ri)
+    self._iterator = _iter
+    self.add_next()
+  
+  def add_next(self, until = None):
+    _iter = self._iterator
+    for x in range(0, 100):
+      value = next(_iter)
+      dict.__setitem__(self, value, { 'used' : False })
+      if until is not None and value >= until:
+        break
+    return value
 
-r_iterator = RIterator()
-riter = iter(r_iterator)
 
-p2 = P2n(2)
-p2iter = p2.get_iterator()
+  def __missing__(self, key):
+    while True:
+      nxt = self.add_next(key)
+      if nxt >= key:
+        break
+    return dict.__getitem__(self, key) if nxt == key else None
 
-p2_1 = P2n_1(2)
-p2n_1iter = p2_1.get_iterator()
 
-r_terms = gen_terms(riter, 10)
-p2n_terms = gen_terms(p2iter, 10)
-p2n_1_terms = gen_terms(p2n_1iter, 10)
