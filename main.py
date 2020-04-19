934b43923bf79f967e5ff5abd56b3323383afa37
deostroll <deostroll@gmail.com>
2020-04-19 09:34:43 +0530

    series terms enumerator

--- main.py
+++ main.py
@@ -89,10 +89,86 @@ def nf2n(p, n):
   return gmpy2.mpz(p) ** 2 ** n
 
 def nf2n_1(p, n):
-  return gmpy2.mpz(p) ** (2 ** n - 1)
+  return gmpy2.mpz(p) ** ((2 ** n) - 1)
 
-def serial_iterator(n = 0):
+def serial_gen(n = 0):
   while True:
     yield n
     n += 1
 
+class NC:
+  """
+  Number container class
+  """
+  display = False
+  threshold = 10 ** 10
+  def __init__(self, n):
+    self.n = n
+  def __str__(self):
+    return str(self.n)
+  def __repr__(self):
+    if NC.display:
+      f = gmpy2.mpfr(self.n)
+      t = gmpy2.digits(f, 10, 5)
+      return '0.%se%s' % (t)
+    elif self.n/NC.threshold < 1:
+      return str(self.n)
+    else:
+      return '<BIG_NUM>'
+
+def series_terms_enumerator(prime, start = 0, list_size = 3):
+  return [ ( \
+              prime,                    \
+              n,
+              NC( nf2n(prime, n) ),     \
+              NC( nf2n_1(prime, n) )    \
+            ) for n in range(start, start + list_size)]
+      
+# =========
+
+# n = 3
+# # p2 = [ ( 2, n, NumContainer(nf2n(2, n)), NumContainer(nf2n_1(2, n)) ) for n in range(0, n + 1 ) ]
+# p2 = [ ( 2, n, nf2n(2, n), nf2n_1(2, n) ) for n in range(0, n + 1 ) ]
+
+# pp(p2)
+
+# x1 = p2[1][3]
+# x2 = p2[2][3]
+# print(list(range(x1, x2)))
+# rdict = RDict()
+# sep = "=" * 10
+# length = len(p2)
+# for x in range(1, len(p2)):
+#   curr = p2[x]
+#   prev = p2[x - 1]
+
+#   low = prev[3]
+#   high = curr[3]
+
+#   if high - low - 1 > 0:
+#     for y in range(low + 1, high):
+#       v = 2 * y
+#       # print(v , rdict[v])
+#       q = rdict[v]
+#       q['used'] = True
+#       q['start'] = low
+#       q['end'] = high
+  
+#   # print(sep)
+
+# # pp(rdict.items())
+# res = filter(lambda t: t[1]['used'] == False, rdict.items())
+# pp(dict(res))
+
+# =================
+p = 2
+p2_0_2 = series_terms_enumerator(p)
+
+pp(p2_0_2)
+
+p2_3_5 = series_terms_enumerator(p, 3)
+
+pp(p2_3_5)
+
+p2_0_5 = series_terms_enumerator(p, list_size=5)
+pp(p2_0_5)
